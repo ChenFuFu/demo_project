ad43aa839a8658e0161020153e383168c3e8d3ae
ChenFuFu <mrchen6893@gmail.com>
2021-08-31 16:57:33 +0800

    提交权限表

---
+++ springbase/src/main/java/com/chen/springbase/entriy/PermissionEntity.java
@@ -0,0 +1,57 @@
+package com.chen.springbase.entriy;
+
+
+/**
+ * @Author Chen
+ * @Date 2021-08-27
+ * @Time 10:02:26
+ * @Description 用户信息
+ **/
+public class PermissionEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 权限名称
+     */
+    private String permission;
+
+    /**
+     * 权限描述
+     */
+    private String desc;
+
+
+    public PermissionEntity() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+}
